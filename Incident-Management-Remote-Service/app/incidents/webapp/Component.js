14629a2ebd616a6e40c3da56960dd4cb952246c5
i329347 <gopal.anand@sap.com>
2025-08-20 19:54:50 +0530

    updates

--- Incident-Management-Remote-Service/app/incidents/webapp/Component.js
+++ Incident-Management-Remote-Service/app/incidents/webapp/Component.js
@@ -1,7 +1,7 @@
 sap.ui.define(
   ["sap/fe/core/AppComponent", "sap/ui/performance/trace/FESR"], function(Component, FESR) {
       "use strict";
-   FESR.setActive(true, new URI(sap.ui.require.toURL("ukrs01.incidents")).path() + "/fesr");
+    FESR.setActive(true, new URI(sap.ui.require.toUrl("ukrs01.incidents")).path() + "/fesr");
       return Component.extend("ukrs01.incidents.Component", {
  	 metadata: {
  	     manifest: "json"
