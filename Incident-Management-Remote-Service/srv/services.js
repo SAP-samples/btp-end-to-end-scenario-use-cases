2b1665ccafc130940674a00a248f98aba1999b6a
i329347 <gopal.anand@sap.com>
2025-08-21 09:58:26 +0530

    Refactor customer handling in ProcessorService to improve data retrieval and processing

--- Incident-Management-Remote-Service/srv/services.js
+++ Incident-Management-Remote-Service/srv/services.js
@@ -13,68 +13,74 @@ class ProcessorService extends cds.ApplicationService {
   }
 
   async onCustomerCache(req, next) {
-    const { Customers } = this.entities;
-    const newCustomerId = req.data.customer_ID;
-    const result = await next();
-    const { BusinessPartner } = this.remoteService.entities;
-    if (newCustomerId && (newCustomerId !== "") && ((req.event == "CREATE") || (req.event == "UPDATE"))) {
-      console.log('>> CREATE or UPDATE customer!');
-
-      // Expands are required as the runtime does not support path expressions for remote services
-      const customer = await this.S4bupa.run(SELECT.one(BusinessPartner, bp => {
-        bp('*'),
-          bp.addresses(address => {
-            address('email', 'phoneNumber'),
-              address.email(emails => {
-                emails('email')
-              }),
-              address.phoneNumber(phoneNumber => {
-                phoneNumber('phone')
-              })
-          })
-      }).where({ ID: newCustomerId }));
-                                                                                    
-      if(customer) {
-        customer.email = customer.addresses[0]?.email[0]?.email;
-        customer.phone = customer.addresses[0]?.phoneNumber[0]?.phone;
-        delete customer.addresses;
-        delete customer.name;
-        await UPSERT.into(Customers).entries(customer);
-      }
-    }
-    return result;
-    }
-    
-  async onCustomerRead(req) {
-    console.log('>> delegating to S4 service...', req.query);
-    const top = parseInt(req._queryOptions?.$top) || 100;
-    const skip = parseInt(req._queryOptions?.$skip) || 0;
-  
-    const { BusinessPartner } = this.remoteService.entities;
+  const { Customers } = this.entities;
+  const newCustomerId = req.data.customer_ID;
+  const result = await next();
+  const { BusinessPartner } = this.remoteService.entities;
+  if (newCustomerId && newCustomerId !== "") {
+    console.log('>> CREATE or UPDATE customer!');
 
     // Expands are required as the runtime does not support path expressions for remote services
-    let result = await this.S4bupa.run(SELECT.from(BusinessPartner, bp => {
-      bp('*'),
+    const customer = await this.S4bupa.run(SELECT.one(BusinessPartner, bp => {
+      bp('*');
         bp.addresses(address => {
-          address('email'),
+          address('email', 'phoneNumber');
             address.email(emails => {
-              emails('email');
+              emails('email')
             });
+            address.phoneNumber(phoneNumber => {
+              phoneNumber('phone')
+            })
         })
-    }).limit(top, skip));
-  
-    result = result.map((bp) => ({
-      ID: bp.ID,
-      name: bp.name,
-      email: bp.addresses[0]?.email[0]?.email
-    }));
+    }).where({ ID: newCustomerId }));
 
-    // Explicitly set $count so the values show up in the value help in the UI
-    result.$count = 1000;
-    console.log("after result", result);
-    return result;
-  }   
+    if(customer) {
+      customer.email = customer.addresses[0]?.email[0]?.email;
+      customer.phone = customer.addresses[0]?.phoneNumber[0]?.phone;
+      delete customer.addresses;
+      delete customer.name;
+      await UPSERT.into(Customers).entries(customer);
+    }
+  }
+  return result;
+}
+    
+async onCustomerRead(req) {
+      console.log('>> delegating to S4 service...', req.query);
+      let { limit, one } = req.query.SELECT
+      if(!limit) limit = { rows: { val: 55 }, offset: { val: 0 } } //default limit to 55 rows
 
+      const { BusinessPartner } = this.remoteService.entities;
+      const query = SELECT.from(BusinessPartner, bp => {
+        bp('*');
+        bp.addresses(address => {
+          address('email');
+          address.email(emails => {
+            emails('email');
+          });
+        });
+      }).limit(limit)
+
+      if(one){
+        // support for single entity read
+        query.where({ ID: req.data.ID });
+      }
+      // Expands are required as the runtime does not support path expressions for remote services
+      let result = await this.S4bupa.run(query);
+
+      result = result.map((bp) => ({
+        ID: bp.ID,
+        name: bp.name,
+        email: (bp.addresses[0]?.email[0]?.email || ''),
+        firstName: bp.firstName,
+        lastName: bp.lastName,
+      }));
+
+      // Explicitly set $count so the values show up in the value help in the UI
+      result.$count = 1000;
+      console.log("after result", result);
+      return result;
+    }   
   changeUrgencyDueToSubject(data) {
     if (data) {
       const incidents = Array.isArray(data) ? data : [data];
