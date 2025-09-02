f3139d6b1fe083ea708b68969affe17d929becc3
I807787 <yevgen.trukhin@sap.com>
2025-09-02 11:28:54 -0400

    UpdateD

--- Incident-Management-Remote-Service/server.js
+++ Incident-Management-Remote-Service/server.js
@@ -1,29 +1,12 @@
- const cds = require('@sap/cds')
- // Import necessary modules
- const { metrics } = require('@opentelemetry/api');
+require('@sap/xotel-agent-ext-js/dist/common/tracer');
+const app = require('express')()
+const fesr = require("@sap/fesr-to-otel-js");
+const autoConf = require('@sap/xotel-agent-ext-js/dist/config/AutoInstrumentationConfig');
 
- const meter = metrics.getMeter('@capire/incidents:incidents.urgency.high');
- // Create a counter
- const counter = meter.createUpDownCounter('incidents.urgency.high');
- require('@sap/xotel-agent-ext-js/dist/common/tracer');
- const app = require('express')()
- const fesr = require("@sap/fesr-to-otel-js");
- cds.serve('all').in(app)
- app.listen()
- cds.on("bootstrap", (app) => fesr.registerFesrEndpoint(app));
-//  cds.on('served', async () => {
-//  const { ProcessorService } = cds.services
-//  // Increase count when new incident with high urgency is created
-//  ProcessorService.after("CREATE", "Incidents", (results, req) => {
-//      if (results.urgency_code === "H" && results.status_code !== "C") {
-//      counter.add(1, { 'sap.tenancy.tenant_id': req.tenant });
-//      }
-//  });
-//  // Reduce count once incident is closed
-//  ProcessorService.after("UPDATE", "Incidents", (results, req) => {
-//      if (results.urgency_code === "H" && results?.status_code === "C") {
-//      counter.add(-1, { 'sap.tenancy.tenant_id': req.tenant });
-//      }
-//  });
-//  });
+
+
+cds.serve('all').in(app)
+app.listen()
+cds.on("bootstrap", (app) => fesr.registerFesrEndpoint(app));
+
  module.exports = cds.server
